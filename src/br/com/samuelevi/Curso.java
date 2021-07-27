cf55c6e77cc0116b18fa7e21df1830de229843da
Samuel Levi Araújo Alves <samuel.levi@senior.com.br>
2021-07-27 11:22:20 -0300

    Relacionamentos entre classes

--- src/br/com/samuelevi/Curso.java
+++ src/br/com/samuelevi/Curso.java
@@ -40,9 +40,13 @@ public class Curso {
 		this.aulas.add(aula);
 	}
 	
+	public int getTempoTotal() {
+		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+	}
+	
 	@Override
 	public String toString() {
-		return "Curso: " + nome + ", instrutor: " + instrutor + ", aulas: " + aulas;
+		return "Curso: " + nome + ", tempo total: " + this.getTempoTotal();
 	}
 
 }
---
+++ src/br/com/samuelevi/TestaCurso2.java
@@ -0,0 +1,32 @@
+package br.com.samuelevi;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TestaCurso2 {
+
+	public static void main(String[] args) {
+		Curso javaColecoes = new Curso("Dominando coleções do java", "Paulo Silveira");
+		
+		javaColecoes.adiciona(new Aula("Trabalhando com Arraylist", 21));
+		javaColecoes.adiciona(new Aula("Probabilidade e estatística", 32));
+		javaColecoes.adiciona(new Aula("Java Collections", 15));
+		javaColecoes.adiciona(new Aula("Java Streams e Lambdas", 49));
+		
+		List<Aula> aulasImutaveis = javaColecoes.getAulas();
+		aulasImutaveis.forEach(System.out::println);
+		
+		List<Aula> aulas = new ArrayList<>(javaColecoes.getAulas());
+		System.out.println();
+		Collections.sort(aulas);
+		aulas.forEach(System.out::println);
+		
+		int sum = aulas.stream().mapToInt(Aula::getTempo).sum();
+		System.out.println("Minutos: " + sum);
+		
+		
+		
+	}
+
+}
