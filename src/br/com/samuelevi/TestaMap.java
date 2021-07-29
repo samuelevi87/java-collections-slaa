783578c53d2ccc107926815b4648f16a5c9494e9
Samuel Levi Araújo Alves <samuel.levi@senior.com.br>
2021-07-29 17:37:29 -0300

    Map <chave, valor>

--- src/br/com/samuelevi/TestaMap.java
+++ src/br/com/samuelevi/TestaMap.java
@@ -1,7 +1,10 @@
 package br.com.samuelevi;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class TestaMap {
 
@@ -17,6 +20,30 @@ public class TestaMap {
 		pessoas.put(18, "Laura Alencar");
 
 		pessoas.keySet().forEach(key -> System.out.println(pessoas.get(key)));
+		System.out.println();
 		
+		/**
+		 * Listando todas as chaves do mapa
+		 */
+		Set<Integer> chaves = pessoas.keySet();    
+		for (Integer key : chaves) {
+		    System.out.println(key);
+		}
+		
+		/**
+		 * Listando todos os valores do mapa
+		 */
+		System.out.println();
+		Collection<String> nomes = pessoas.values();
+		nomes.forEach(System.out::println);
+		
+		/**
+		 * Listando as associações de <chave, valor> existentes no mapa.
+		 */
+		System.out.println();
+		Set<Entry<Integer, String>> associacoes = pessoas.entrySet();
+		for (Entry<Integer, String> associacao : associacoes) {
+			System.out.println(associacao.getKey() + " " + associacao.getValue());			
+		}
 	}
 }
